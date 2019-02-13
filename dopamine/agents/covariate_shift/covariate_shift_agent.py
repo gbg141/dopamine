2232c2ea725a6c9d1cb8815889dc72b054842e42
gbg141 <guille_gbg@hotmail.com>
2019-02-13 13:13:27 +0100

    Initial commit Covariate Shift Agent

---
+++ dopamine/agents/covariate_shift/covariate_shift_agent.py
@@ -0,0 +1,384 @@
+# coding=utf-8
+# gbg141
+
+"""Agent permorming COP-TD
+
+Details in "Off-Policy Deep Reinforcement Learning by Bootstrapping the Covariate Shift" by 
+Carles Gelada & Marc G. Bellemare (2018)
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+
+
+from dopamine.agents.dqn import dqn_agent
+from dopamine.agents.rainbow import rainbow_agent
+from dopamine.replay_memory import prioritized_replay_buffer, cs_replay_buffer
+import numpy as np
+import tensorflow as tf
+
+import gin.tf
+
+slim = tf.contrib.slim
+
+
+@gin.configurable
+class CovariateShiftAgent(rainbow_agent.RainbowAgent):
+  """A compact implementation of a simplified Rainbow agent."""
+
+  def __init__(self,
+               sess,
+               num_actions,
+               observation_shape=dqn_agent.NATURE_DQN_OBSERVATION_SHAPE,
+               observation_dtype=dqn_agent.NATURE_DQN_DTYPE,
+               stack_size=dqn_agent.NATURE_DQN_STACK_SIZE,
+               num_atoms=51,
+               vmax=10.,
+               gamma=0.99,
+               update_horizon=1,
+               min_replay_history=20000,
+               update_period=4,
+               target_update_period=8000,
+               epsilon_fn=dqn_agent.linearly_decaying_epsilon,
+               epsilon_train=1.0,
+               epsilon_eval=0.1,
+               epsilon_decay_period=250000,
+               replay_scheme='uniform',
+               tf_device='/cpu:*',
+               use_staging=True,
+               optimizer=tf.train.AdamOptimizer(
+                   learning_rate=0.00025, epsilon=0.0003125),
+               summary_writer=None,
+               summary_writing_frequency=500,
+               ratio_num_atoms=51,
+               ratio_cmin=0.,
+               ratio_cmax=10.,
+               ratio_discount_factor=0.99,
+               ratio_loss_weight=0.02):
+    """Initializes the agent and constructs the components of its graph.
+
+    Args:
+      sess: `tf.Session`, for executing ops.
+      num_actions: int, number of actions the agent can take at any state.
+      observation_shape: tuple of ints or an int. If single int, the observation
+        is assumed to be a 2D square.
+      observation_dtype: tf.DType, specifies the type of the observations. Note
+        that if your inputs are continuous, you should set this to tf.float32.
+      stack_size: int, number of frames to use in state stack.
+      num_atoms: int, the number of buckets of the value function distribution.
+      vmax: float, the value distribution support is [-vmax, vmax].
+      gamma: float, discount factor with the usual RL meaning.
+      update_horizon: int, horizon at which updates are performed, the 'n' in
+        n-step update.
+      min_replay_history: int, number of transitions that should be experienced
+        before the agent begins training its value function.
+      update_period: int, period between DQN updates.
+      target_update_period: int, update period for the target network.
+      epsilon_fn: function expecting 4 parameters:
+        (decay_period, step, warmup_steps, epsilon). This function should return
+        the epsilon value used for exploration during training.
+      epsilon_train: float, the value to which the agent's epsilon is eventually
+        decayed during training.
+      epsilon_eval: float, epsilon used when evaluating the agent.
+      epsilon_decay_period: int, length of the epsilon decay schedule.
+      replay_scheme: str, 'prioritized' or 'uniform', the sampling scheme of the
+        replay memory.
+      tf_device: str, Tensorflow device on which the agent's graph is executed.
+      use_staging: bool, when True use a staging area to prefetch the next
+        training batch, speeding training up by about 30%.
+      optimizer: `tf.train.Optimizer`, for training the value function.
+      summary_writer: SummaryWriter object for outputting training statistics.
+        Summary writing disabled if set to None.
+      summary_writing_frequency: int, frequency with which summaries will be
+        written. Lower values will result in slower training.
+      ratio_num_atoms: int, the number of buckets of the ratio function distribution.
+      ratio_cmin: float, the predefined minimum ratio value
+      ratio_cmax: float, the predefined maximum ratio value
+      ratio_discount_factor: float, discount factor used in Discounted COP-TD
+      ratio_loss_weight: float, loss weight of the covariate shift ratio estimation
+    """
+    # We need this because some tools convert round floats into ints.
+    vmax = float(vmax)
+    self._num_atoms = num_atoms
+    self._support = tf.linspace(-vmax, vmax, num_atoms)
+    self._replay_scheme = replay_scheme
+    self.optimizer = optimizer
+
+    # Initializing extra parameters
+    ratio_cmin = float(ratio_cmin)
+    ratio_cmax = float(ratio_cmax)
+    self._ratio_num_atoms = ratio_num_atoms
+    self._ratio_support = tf.linspace(ratio_cmin, ratio_cmax, ratio_num_atoms)
+    self.ratio_discount_factor = ratio_discount_factor
+    self.ratio_loss_weight = ratio_loss_weight
+
+    super(CovariateShiftAgent, self).__init__(
+          sess=sess,
+          num_actions=num_actions,
+          observation_shape=observation_shape,
+          observation_dtype=observation_dtype,
+          stack_size=stack_size,
+          gamma=gamma,
+          update_horizon=update_horizon,
+          min_replay_history=min_replay_history,
+          update_period=update_period,
+          target_update_period=target_update_period,
+          epsilon_fn=epsilon_fn,
+          epsilon_train=epsilon_train,
+          epsilon_eval=epsilon_eval,
+          epsilon_decay_period=epsilon_decay_period,
+          tf_device=tf_device,
+          use_staging=use_staging,
+          optimizer=self.optimizer,
+          summary_writer=summary_writer,
+          summary_writing_frequency=summary_writing_frequency)
+
+  def begin_episode(self, observation):
+    """Returns the agent's first action for this episode.
+
+    Args:
+      observation: numpy array, the environment's initial observation.
+
+    Returns:
+      int, the selected action.
+    """
+    self._reset_state()
+    self._record_observation(observation)
+
+    if not self.eval_mode:
+      self._train_step()
+
+    self.action = self._select_action()
+    return self.action
+
+  def step(self, reward, observation):
+    """Records the most recent transition and returns the agent's next action.
+
+    We store the observation of the last time step since we want to store it
+    with the reward.
+
+    Args:
+      reward: float, the reward received from the agent's most recent action.
+      observation: numpy array, the most recent observation.
+
+    Returns:
+      int, the selected action.
+    """
+    self._last_observation = self._observation
+    self._record_observation(observation)
+
+    if not self.eval_mode:
+      self._store_transition(self._last_observation, self.action, reward, False)
+      self._train_step()
+
+    self.action = self._select_action()
+    return self.action
+
+  def _get_network_type(self):
+    """Returns the type of the outputs of a value distribution network.
+
+    Returns:
+      net_type: _network_type object defining the outputs of the network.
+    """
+    return collections.namedtuple('cs_network',
+                                  ['c_values', 'c_logits', 'c_probabilities',
+                                   'q_values', 'logits', 'probabilities'])
+
+  def _network_template(self, state):
+    """Builds a convolutional network that outputs CS ratio and Q-value distributions.
+
+    Args:
+      state: `tf.Tensor`, contains the agent's current state.
+
+    Returns:
+      net: _network_type object containing the tensors output by the network.
+    """
+    weights_initializer = slim.variance_scaling_initializer(
+        factor=1.0 / np.sqrt(3.0), mode='FAN_IN', uniform=True)
+
+    net = tf.cast(state, tf.float32)
+    net = tf.div(net, 255.)
+    net = slim.conv2d(
+        net, 32, [8, 8], stride=4, weights_initializer=weights_initializer)
+    net = slim.conv2d(
+        net, 64, [4, 4], stride=2, weights_initializer=weights_initializer)
+    net = slim.conv2d(
+        net, 64, [3, 3], stride=1, weights_initializer=weights_initializer)
+    net = slim.flatten(net)
+
+    ratio_net = slim.fully_connected(
+        net, 512, weights_initializer=weights_initializer)
+    ratio_net = slim.fully_connected(
+        ratio_net,
+        self._ratio_num_atoms,
+        activation_fn=None,
+        weights_initializer=weights_initializer)
+    
+    net = slim.fully_connected(
+        net, 512, weights_initializer=weights_initializer)
+    net = slim.fully_connected(
+        net,
+        self.num_actions * self._num_atoms,
+        activation_fn=None,
+        weights_initializer=weights_initializer)
+
+    c_logits = tf.reshape(ratio_net, [-1, 1, self._ratio_num_atoms])
+    c_probabilities = tf.contrib.layers.softmax(c_logits)
+    c_values = tf.reduce_sum(self._ratio_support * c_probabilities, axis=2)
+
+    logits = tf.reshape(net, [-1, self.num_actions, self._num_atoms])
+    probabilities = tf.contrib.layers.softmax(logits)
+    q_values = tf.reduce_sum(self._support * probabilities, axis=2)
+    return self._get_network_type()(c_values, c_logits, c_probabilities, q_values, logits, probabilities)
+
+  def _build_networks(self):
+    """Extends the build networks computations with:
+
+      self._replay_next_net_outputs: The replayed next states' values.
+      self._replay_target_net_outputs: The replayed states' target
+        values.
+    """
+    super(CovariateShiftAgent, self)._build_networks()
+
+    self._replay_next_net_outputs = self.online_convnet(self._replay.next_states)
+    self._replay_target_net_outputs = self.target_convnet(self._replay.states)
+
+  def _build_replay_buffer(self, use_staging):
+    """Creates the replay buffer used by the agent.
+
+    Args:
+      use_staging: bool, if True, uses a staging area to prefetch data for
+        faster training.
+
+    Returns:
+      A `WrappedCSReplayBuffer` object.
+
+    Raises:
+      ValueError: if given an invalid replay scheme.
+    """
+    return cs_replay_buffer.WrappedCSReplayBuffer(
+        observation_shape=self.observation_shape,
+        stack_size=self.stack_size,
+        use_staging=use_staging,
+        update_horizon=self.update_horizon,
+        gamma=self.gamma)
+
+  def compute_policies_quotient(self):
+    '''Computes the quotient of policies that appears at the DCOP update
+
+    Returns:
+      policies_quotient: tf.tensor, the quotient from the replay
+    '''
+    batch_size = self._replay.batch_size
+    qt_argmax_actions = tf.argmax(self._replay_target_net_outputs.q_values, axis=1)[:, None]
+    qt_argmax_actions = tf.reshape(qt_argmax_actions, [batch_size,1])
+    replay_actions = tf.reshape(self._replay.actions, [batch_size,1])
+
+    coincidences = tf.equal(qt_argmax_actions, replay_actions)
+
+    coincidence_quotient = tf.fill([batch_size,1], self.num_actions * (1 - self.epsilon_eval))
+    no_coincidence_quotient = tf.fill([batch_size,1], self.epsilon_eval)
+
+    policies_quotient = tf.where(coincidences, coincidence_quotient, no_coincidence_quotient)
+    return policies_quotient
+
+  def _build_target_c_distribution(self):
+    """Builds the c ratio target distribution.
+
+    First, we compute the support of the target, gamma * (pi/mu) * x + (1-gamma), Where x
+    is the support of the previous state distribution:
+
+      * Evenly spaced in [vmin, vmax] if the current state is nonbegginer;
+      * 1 otherwise (duplicated ratio_num_atoms times).
+
+    Second, we compute the cs ratio probabilities of the current state.
+
+    Finally we project the target (support + probabilities) onto the
+    original support.
+
+    Returns:
+      target_distribution: tf.tensor, the target distribution from the replay.
+    """
+    batch_size = self._replay.batch_size
+
+    # size of tiled_support: batch_size x ratio_num_atoms
+    tiled_support = tf.tile(self._ratio_support, [batch_size])
+    tiled_support = tf.reshape(tiled_support, [batch_size, self._ratio_num_atoms])
+
+    # size of target_support: batch_size x ratio_num_atoms
+
+    is_beginning_multiplier = 1. - tf.cast(self._replay.terminals, tf.float32)
+    # Incorporate beginning state to ratio discount factor.
+    # size of gamma_with_beginning: batch_size x 1
+    gamma_with_beginning = self.ratio_discount_factor * is_beginning_multiplier
+    gamma_with_beginning = gamma_with_beginning[:, None]
+
+    # Compute the quotient of policies
+    policies_quotient = self.compute_policies_quotient()
+
+    # Addition of the constant term, 1-discount factor
+    constant_term = tf.tile(1. - gamma_with_beginning, [1, self._ratio_num_atoms])
+
+    target_support = gamma_with_beginning * policies_quotient * tiled_support + constant_term
+
+    # size of next_probabilities: batch_size x ratio_num_atoms
+    probabilities = self._replay_target_net_outputs.c_probabilities
+
+    return rainbow_agent.project_distribution(target_support, probabilities,
+                                              self._ratio_support)
+  
+  def _build_train_op(self):
+    """Builds a training op.
+
+    Returns:
+      train_op: An op performing one step of training from replay data.
+    """
+    # Loss of the ratio model
+    c_target_distribution = tf.stop_gradient(self._build_target_c_distribution())
+
+    c_logits = self._replay_next_net_outputs.c_logits
+
+    c_loss = tf.nn.softmax_cross_entropy_with_logits(
+        labels=c_target_distribution,
+        logits=c_logits)
+    c_loss = tf.scalar_mul(self.ratio_loss_weight, c_loss)
+
+    # Loss os the Q model
+    target_distribution = tf.stop_gradient(self._build_target_distribution())
+
+    # size of indices: batch_size x 1.
+    indices = tf.range(tf.shape(self._replay_net_outputs.logits)[0])[:, None]
+    # size of reshaped_actions: batch_size x 2.
+    reshaped_actions = tf.concat([indices, self._replay.actions[:, None]], 1)
+    # For each element of the batch, fetch the logits for its selected action.
+    chosen_action_logits = tf.gather_nd(self._replay_net_outputs.logits,
+                                        reshaped_actions)
+
+    loss = tf.nn.softmax_cross_entropy_with_logits(
+        labels=target_distribution,
+        logits=chosen_action_logits)
+
+    probs = self._replay.transition['sampling_probabilities']
+    loss_weights = 1.0 / tf.sqrt(probs + 1e-10)
+    loss_weights /= tf.reduce_max(loss_weights)
+
+    update_priorities_op = self._replay.tf_set_priority(
+          self._replay.indices, self._replay_net_outputs.c_values)
+    
+    # Weight the loss by the inverse priorities.
+    loss = loss_weights * loss
+
+    # Generate the final loss
+    final_loss = loss + c_loss
+
+    with tf.control_dependencies([update_priorities_op]):
+      if self.summary_writer is not None:
+        with tf.variable_scope('Losses'):
+          tf.summary.scalar('CrossEntropyLoss', tf.reduce_mean(loss))
+      # Schaul et al. reports a slightly different rule, where 1/N is also
+      # exponentiated by beta. Not doing so seems more reasonable, and did not
+      # impact performance in our experiments.
+      return self.optimizer.minimize(tf.reduce_mean(loss)), loss
---
+++ dopamine/replay_memory/cs_replay_buffer.py
@@ -0,0 +1,205 @@
+# coding=utf-8
+# Copyright 2018 The Dopamine Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""An implementation of Prioritized Experience Replay (PER).
+
+This implementation is based on the paper "Prioritized Experience Replay"
+by Tom Schaul et al. (2015). Many thanks to Tom Schaul, John Quan, and Matteo
+Hessel for providing useful pointers on the algorithm and its implementation.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import gzip
+
+from dopamine.replay_memory import circular_replay_buffer, prioritized_replay_buffer
+from dopamine.replay_memory import sum_tree
+from dopamine.replay_memory.circular_replay_buffer import ReplayElement
+import numpy as np
+import tensorflow as tf
+
+import gin.tf
+
+
+@gin.configurable(blacklist=['observation_shape', 'stack_size',
+                             'update_horizon', 'gamma'])
+class WrappedCSReplayBuffer(
+    prioritized_replay_buffer.WrappedPrioritizedReplayBuffer):
+  """Wrapper of OutOfGraphPrioritizedReplayBuffer with both uniform and
+  in-graph sampling.
+
+  Usage:
+
+    * To add a transition:  Call the add function.
+
+    * To sample a batch:  Query any of the tensors in the transition dictionary.
+                          Every sess.run that requires any of these tensors will
+                          sample a new transition.
+  """
+
+  def __init__(self,
+               observation_shape,
+               stack_size,
+               use_staging=True,
+               replay_capacity=1000000,
+               batch_size=32,
+               update_horizon=1,
+               gamma=0.99,
+               max_sample_attempts=circular_replay_buffer.MAX_SAMPLE_ATTEMPTS,
+               extra_storage_types=None,
+               observation_dtype=np.uint8):
+    """Initializes WrappedPrioritizedReplayBuffer.
+
+    Args:
+      observation_shape: tuple of ints.
+      stack_size: int, number of frames to use in state stack.
+      use_staging: bool, when True it would use a staging area to prefetch
+        the next sampling batch.
+      replay_capacity: int, number of transitions to keep in memory.
+      batch_size: int.
+      update_horizon: int, length of update ('n' in n-step update).
+      gamma: int, the discount factor.
+      max_sample_attempts: int, the maximum number of attempts allowed to
+        get a sample.
+      extra_storage_types: list of ReplayElements defining the type of the extra
+        contents that will be stored and returned by sample_transition_batch.
+      observation_dtype: np.dtype, type of the observations. Defaults to
+        np.uint8 for Atari 2600.
+
+    Raises:
+      ValueError: If update_horizon is not positive.
+      ValueError: If discount factor is not in [0, 1].
+    """
+    super(WrappedCSReplayBuffer, self).__init__(
+        observation_shape,
+        stack_size,
+        use_staging,
+        replay_capacity,
+        batch_size,
+        update_horizon,
+        gamma,
+        extra_storage_types=extra_storage_types)
+
+  def create_sampling_ops(self, use_staging):
+    """Creates the ops necessary to sample from the replay buffer.
+
+    Creates the transition dictionary containing the sampling tensors.
+
+    Args:
+      use_staging: bool, when True it would use a staging area to prefetch
+        the next sampling batch.
+    """
+    with tf.name_scope('sample_replay'):
+      with tf.device('/cpu:*'):
+        transition_type = self.memory.get_transition_elements()
+        transition_tensors = tf.py_func(
+            self.memory.sample_transition_batch, [],
+            [return_entry.type for return_entry in transition_type],
+            name='replay_sample_py_func')
+        uniform_sample_indices = tf.py_func(
+            super(prioritized_replay_buffer.OutOfGraphPrioritizedReplayBuffer, self.memory).
+            sample_index_batch, [self.batch_size], [np.int32],
+            name='uniform_index_sampling') 
+        uniform_transition_tensors = tf.py_func(
+            self.memory.sample_transition_batch, [uniform_sample_indices],
+            [return_entry.type for return_entry in transition_type],
+            name='uniform_replay_sample_py_func')
+        self._set_transition_shape(transition_tensors, transition_type)
+        self._set_transition_shape(uniform_transition_tensors, transition_type)
+        if use_staging:
+          transition_tensors, uniform_transition_tensors = self._set_up_staging(transition_tensors, uniform_transition_tensors)
+          self._set_transition_shape(transition_tensors, transition_type)
+          self._set_transition_shape(uniform_transition_tensors, transition_type)
+
+        # Unpack sample transition into member variables.
+        self.unpack_transition(transition_tensors, uniform_transition_tensors, transition_type)
+
+  def _set_up_staging(self, transition, u_transition):
+    """Sets up staging ops for prefetching the next transition.
+
+    This allows us to hide the py_func latency. To do so we use a staging area
+    to pre-fetch the next batch of transitions.
+
+    Args:
+      transition: tuple of tf.Tensors with shape
+        memory.get_transition_elements().
+      u_transition: tuple of tf.Tensors with shape
+        memory.get_transition_elements().
+
+    Returns:
+      prefetched_transition: tuple of tf.Tensors with shape
+        memory.get_transition_elements() that have been previously prefetched.
+    """
+    transition_type = self.memory.get_transition_elements()
+
+    # Create the staging area in CPU.
+    prefetch_area = tf.contrib.staging.StagingArea(
+        [shape_with_type.type for shape_with_type in transition_type])
+    u_prefetch_area = tf.contrib.staging.StagingArea(
+        [shape_with_type.type for shape_with_type in transition_type])
+
+    # Store prefetch op for tests, but keep it private -- users should not be
+    # calling _prefetch_batch.
+    self._prefetch_batch = prefetch_area.put(transition)
+    initial_prefetch = tf.cond(
+        tf.equal(prefetch_area.size(), 0),
+        lambda: prefetch_area.put(transition), tf.no_op)
+    
+    self._u_prefetch_batch = u_prefetch_area.put(transition)
+    u_initial_prefetch = tf.cond(
+        tf.equal(u_prefetch_area.size(), 0),
+        lambda: u_prefetch_area.put(u_transition), tf.no_op)
+
+    # Every time a transition is sampled self.prefetch_batch will be
+    # called. If the staging area is empty, two put ops will be called.
+    with tf.control_dependencies([self._prefetch_batch, initial_prefetch, 
+                                  self._u_prefetch_batch, u_initial_prefetch]):
+      prefetched_transition = prefetch_area.get()
+      u_prefetched_transition = u_prefetch_area.get()
+
+    return prefetched_transition, u_prefetched_transition
+
+  def unpack_transition(self, transition_tensors, uniform_transition_tensors, transition_type):
+    """Unpacks the given transition into member variables.
+
+    Args:
+      transition_tensors: tuple of tf.Tensors.
+      uniform_transition_tensors: tuple of tf.Tensors.
+      transition_type: tuple of ReplayElements matching transition_tensors.
+    """
+    self.transition = collections.OrderedDict()
+    for element, element_type in zip(transition_tensors, transition_type):
+      self.transition[element_type.name] = element
+
+    # TODO(bellemare): These are legacy and should probably be removed in
+    # future versions.
+    self.states = self.transition['state']
+    self.actions = self.transition['action']
+    self.rewards = self.transition['reward']
+    self.next_states = self.transition['next_state']
+    self.terminals = self.transition['terminal']
+    self.indices = self.transition['indices']
+
+    self.uniform_transition = collections.OrderedDict()
+    for element, element_type in zip(uniform_transition_tensors, transition_type):
+      self.uniform_transition[element_type.name] = element
+
+    self.u_states = self.uniform_transition['state']
+    self.u_actions = self.uniform_transition['action']
+    self.u_rewards = self.uniform_transition['reward']
+    self.u_next_states = self.uniform_transition['next_state']
+    self.u_terminals = self.uniform_transition['terminal']
+    self.u_indices = self.uniform_transition['indices']  
