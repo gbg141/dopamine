1d3aa646e83786f5ea323bc23a7d2eaba3df91eb
gbg141 <guille_gbg@hotmail.com>
2019-03-28 15:30:52 +0100

    Change in run.sh

--- run.sh
+++ run.sh
@@ -3,16 +3,16 @@
 # Script to prepare the docker container to be run
 
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=2
 
-cd /home/dopamine/
 base=./logs
+port=6006
 
 if [ ! -z $1 ]
 then
     python3 -um dopamine.discrete_domains.train --base_dir=$base --debugger --gin_files='dopamine/agents/covariate_shift/configs/covariate_shift.gin' &
-    tensorboard --logdir /home/dopamine/logs --host 0.0.0.0 --debugger_port 7000 &
+    tensorboard --logdir $base --host 0.0.0.0 --port $port --debugger_port 7000 &
 else
     python3 -um dopamine.discrete_domains.train --base_dir=$base --gin_files='dopamine/agents/covariate_shift/configs/covariate_shift.gin' &
-    tensorboard --logdir /home/dopamine/logs --host 0.0.0.0 &
+    tensorboard --logdir $base --host 0.0.0.0 --port $port &
 fi
